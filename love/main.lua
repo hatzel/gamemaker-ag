dfa72b09f597eed19d9857e7c455fb8ac83a8d80
Hans Ole Hatzel <hhatzel@gmail.com>
2016-10-31 22:55:29 +0100

    lol

--- love/main.lua
+++ love/main.lua
@@ -1,52 +1,63 @@
 function love.load()
-    man = love.graphics.newImage('images/man.png')
-    log = love.graphics.newImage('images/tree.png')
+    man = love.graphics.newImage("images/man.png")
+    log = love.graphics.newImage("images/tree.png")
     side = "right"
 
-    logs = {}
-    for i = 0, 3, 1 do
-        generate_log()
-    end
+    restart()
 end
 
-function love.update()
+function restart()
+    logs = {}
+    for i = 1, 3, 1 do
+        generate_log()
+    end
 
+    score = 0
 end
 
 function love.draw()
     love.graphics.clear(255, 255, 255)
 
-    local position_x
-    local scale_x
-    if side == "right" then
-        position_x = 450
-        scale_x = 1
-    elseif side == "left" then
+    local position_x = 450
+    local scale_x = 1
+
+    if side == "left" then
         position_x = 350
         scale_x = -1
     end
-    love.graphics.draw(man, position_x, 400, 0, scale_x, 1)
+
+    love.graphics.draw(man, position_x, 250, 0, scale_x, 1)
 
     for i, direction in pairs(logs) do
-        love.graphics.draw(log, 200, 500 - (i * 200))
+        local scale_x = 1
+        if direction == "left" then
+            scale_x = -1
+        end
+        love.graphics.draw(log, 400, 500 - (i * 200), 0, scale_x, 1, log:getWidth()/2)
     end
+
+    love.graphics.print(score, 20, 20)
 end
 
 function love.keypressed(key)
    if key == "right" then
        side = "right"
+       chop()
    elseif key == "left" then
        side = "left"
+       chop()
    end
 end
 
-function chop(side)
+function chop()
     if side == logs[1] then
-        -- lose score
+        score = score - 1
+        restart()
     else 
-        -- gain score
+        score = score + 1
         table.remove(logs, 1)
     end
+    generate_log()
 end
 
 function generate_log()
